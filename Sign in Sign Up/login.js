6e5e785e7664634c37925cd841c3abbcff31eee6
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-10-13 14:48:40 -0300

    Inicio de algumas validações

--- Sign in Sign Up/login.js
+++ Sign in Sign Up/login.js
@@ -14,10 +14,44 @@ loginBtn.addEventListener('click', () =>{
     container.classList.remove("active");
 })
 
+
+
+//Validações no campo de login inicial
+
+const emailInput = document.querySelector("#input-email");
+const senhaInput = document.querySelector("#input-senha");
+
+
 logarNaApp.addEventListener("click", function(event){
     event.preventDefault();
-    window.location.href="/Tela Principal/telaPrincipal.html"
+
+    let email = emailInput.value;
+    let senha = senhaInput.value;
+
+    if(email == "" || senha == ""){
+        alert("Não deixe o email e a senha vazias")
+    }
+
+    if(validarEmail(email) == false){
+        alert("Email invalido");
+    }
+
+    //window.location.href="/Tela Principal/telaPrincipal.html"
 
 })
+
+
+//validar email
+
+function validarEmail(email){
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
+
+    return regex.test(email);
+}
+
+
+//Fim das validações no campo de login inicial 
+
+
 
 
