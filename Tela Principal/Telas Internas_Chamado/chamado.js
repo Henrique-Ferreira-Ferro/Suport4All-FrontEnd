16bebfe8a8c79837a7ec9be113a0bb1340ecefaf
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-10-15 17:13:44 -0300

    Implementação de funcionalidade que separa os arquivos javascript

--- Tela Principal/Telas Internas_Chamado/chamado.js
+++ Tela Principal/Telas Internas_Chamado/chamado.js
@@ -1,15 +1,33 @@
 
-    /*
-     * código referente a abertura de um chamado! 
-     * 
-     */
-
     const titulo = document.querySelector("#titulo-input");
-
     const btnAbrir = document.querySelector("#btn-abrir");
-
+    const containerTituloInsert = document.querySelector(".container-div-input-titulo");
+    let ElementeSpan;
+        
     btnAbrir.addEventListener("click", function(){
-        console.log(titulo);
-        alert("teste de botão clicado");
+ 
+        if(ElementeSpan){
+            ElementeSpan.remove();
+        }
+
+        let tituloValue = titulo.value;
+        if(tituloValue.trim() == ""){
+            ElementeSpan = document.createElement("span");
+            ElementeSpan.innerHTML = "Não deixe o titulo vaziu!"
+            containerTituloInsert.appendChild(ElementeSpan);
+            ElementeSpan.setAttribute("class", "error");
+            
+        }
+    
+    });    
+
+    titulo.addEventListener("input", function(){
+        ElementeSpan.remove();    
     })
+    
+
+    //Inicio da comunicação com o banco de dados
+
+    
+
 
