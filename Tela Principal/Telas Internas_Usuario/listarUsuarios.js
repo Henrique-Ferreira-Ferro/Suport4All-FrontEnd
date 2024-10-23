2740589e570a1bd8c50b6fa5c5a4bceb5d8eb0d7
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-10-23 19:01:26 -0300

    modificações no css em listar usuarios!

--- Tela Principal/Telas Internas_Usuario/listarUsuarios.js
+++ Tela Principal/Telas Internas_Usuario/listarUsuarios.js
@@ -1,4 +1,9 @@
+//Desativar usuario
 const btnDesativar = document.querySelector("#btnDelete");
+
+//Ativar usuario
+const btnAtivarUsuario = document.querySelector("#btnAtivar");
+
 
 
 const boxDialog = document.querySelector('#box-dialog');
@@ -7,12 +12,21 @@ const btnNao = document.querySelector("#btnNao");
 
 
 
+btnAtivarUsuario.addEventListener("click", function(){
+    alert("usuario ativado com sucesso!");
+})
+
+
 btnDesativar.addEventListener("click", function(){
 
 
     boxDialog.showModal();
 })
 
+
+btnSim.addEventListener("click", function(){
+    
+})
 
 btnNao.addEventListener("click", function(){
     boxDialog.close();
